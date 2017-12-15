4b2f9e37a3f69f3b7e0a12a4a32dfd3ffb97116e
Frank Wang <frank.wang@tenzing.co.nz>
2017-12-15 15:01:08 +1300

    update the merge tag command

--- src/commands/AddMergeTagCommand.js
+++ src/commands/AddMergeTagCommand.js
@@ -52,11 +52,11 @@ define(function () {
     container.appendChild(btnImp);
 
     btnImp.onclick = () => {
-      alert("Add merge tag button pressed");
       // todo: Saving the merge tag
+      let kVal = tb_key.value;
+      let vVal = "{{ " + tb_value.value + " }}";
 
-
-
+      alert("New merge tag -  " + kVal + ": " + vVal + " is saved!");
 
       editor.Modal.close();
     };
